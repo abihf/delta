ebb0c8d8d6597c154cdf8d5920d9e77b20fb3a9b
Abi Hafshin <abi@hafs.in>
2019-02-20 00:45:17 +0700

    fix res_test

--- res_test.go
+++ res_test.go
@@ -1,24 +1,19 @@
 package delta
 
 import (
-	"gotest.tools/assert"
-	"bytes"
-	"reflect"
+	"fmt"
 	"testing"
 
-	"github.com/aws/aws-lambda-go/events"
+	"gotest.tools/assert"
 )
 
 func TestResponseWriter_ToAPIGWProxyResponse(t *testing.T) {
-  w := NewResponseWriter()
-  w.Header().Set("X-Powered-By", "Go")
-  fmt.Fprintf(w, "Hello %s!", "world")
+	w := NewResponseWriter()
+	w.Header().Set("X-Powered-By", "Go")
+	fmt.Fprintf(w, "Hello %s!", "world")
 
-  agres := w.ToAPIGWProxyResponse()
-  
-  // by default status code is 200
-  assert.Equal(t, agres.StatusCode, 200)
+	agres := w.ToAPIGWProxyResponse()
 
-  // 
-  assert.Equal(t, agres.)
+	// by default status code is 200
+	assert.Equal(t, agres.StatusCode, 200)
 }
